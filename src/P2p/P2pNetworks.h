c977076a3d2b60d5e124de9b00cf2d7a81647a9c
pablo <pablo@gmx.es>
2017-10-29 04:57:17 +0100

    alpha

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x06, 0x13, 0x27, 0x32, 0x33, 0x44, 0xdf, 0xff, 0xab, 0xaa, 0x00, 0x18, 0x07, 0x84, 0x01, 0x10 } };
+}
